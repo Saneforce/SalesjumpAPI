8fd46616cdc22c2050db4fe6c8312815ecf1fef7
neelakkannan123@gmail.com <neelakkannan123@gmail.com>
2023-10-19 17:43:41 +0530

    payment status

--- Controllers/jiopaymentController.cs
+++ Controllers/jiopaymentController.cs
@@ -7,6 +7,7 @@ using System.Text;
 using WebApplicationApi.Models;
 using Newtonsoft.Json;
 using System.Text.Json;
+using System.Web.Http.Cors;
 
 namespace WebApplicationApi.Controllers
 {
@@ -26,10 +27,10 @@ namespace WebApplicationApi.Controllers
         }
         [HttpPost]
         [Route("jiopaymentint")]
-       
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
         public async Task<string?> GetpaymentgateAsync([FromBody] Paymentintegrate _Paymentintegrate)
         {
-           // List<Paymentintegrate> TOHDetails = new List<Paymentintegrate>();
+            // List<Paymentintegrate> TOHDetails = new List<Paymentintegrate>();
             Paymentintegrate od = new Paymentintegrate();
 
             //od = new Paymentintegrate();
@@ -39,11 +40,11 @@ namespace WebApplicationApi.Controllers
             var Cus_Name = _Paymentintegrate.Cus_Name;
             var mail_Id = _Paymentintegrate.mail_Id;
             var Describ = _Paymentintegrate.Describ;
-            
 
 
 
-           // TOHDetails.Add(od);
+
+            // TOHDetails.Add(od);
 
             var requestedUrl = "https://pp-apig.jiomoney.com/jfs/v1/app/authenticate";
 
@@ -169,7 +170,75 @@ namespace WebApplicationApi.Controllers
             {
                 return null;
             }
-            
+
+        }
+        [HttpPost]
+        [Route("jiopaymentstatus")]
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public async Task<string?> GetpaymentstatusAsync([FromBody] Jiopaymentstatus _Jiopaymentstatus)
+        {
+            Jiopaymentstatus od = new Jiopaymentstatus();
+            var merchant_Id = _Jiopaymentstatus.merchantId;
+            var merchantLink_Id = _Jiopaymentstatus.merchantLinkId;
+            var jioLink_Id = _Jiopaymentstatus.jioLinkId;
+            var requestedUrl = "https://pp-apig.jiomoney.com/jfs/v1/app/authenticate";
+            var json = @"{
+                ""application"": {
+                    ""clientId"": ""93b0534f5143d7c7d22c5c85a7ac5e6c""
+                },
+                ""authenticateList"": [
+                    {
+                        ""mode"": 22,
+                        ""value"": ""018ca62c40cba5a5cb0e557221631fd1ddcf5570539ac825551d3435e322af41""
+                    }
+                ],
+                ""scope"": ""SESSION"",
+                ""purpose"": 2
+            }";
+            _httpClient.DefaultRequestHeaders.Add("x-trace-id", "01c570cf-2bdf-49d0-a126-baec7038bbd1");
+            //_httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            var requestedData = new StringContent(json, Encoding.UTF8, "application/json");
+            var responses = await _httpClient.PostAsync(requestedUrl, requestedData);
+            if (responses.IsSuccessStatusCode)
+            {
+                var responsesContent = await responses.Content.ReadAsStringAsync();
+                var jsonResponse = System.Text.Json.JsonSerializer.Deserialize<JsonElement>(responsesContent);
+                if (jsonResponse.TryGetProperty("session", out JsonElement sessionElement))
+                {
+                    if (sessionElement.TryGetProperty("accessToken", out JsonElement accessTokenElement))
+                    {
+                        if (accessTokenElement.TryGetProperty("tokenValue", out JsonElement tokenValueElement))
+                        {
+                            var tokenValue = tokenValueElement.GetString();
+                            _httpClient.DefaultRequestHeaders.Add("x-app-access-token", tokenValue);
+
+                        }
+                    }
+                    if (sessionElement.TryGetProperty("appIdentifierToken", out JsonElement appIdentifierTokenElement))
+                    {
+                        var appIdentifierToken = appIdentifierTokenElement.GetString();
+                        _httpClient.DefaultRequestHeaders.Add("x-appid-token", appIdentifierToken);
+
+                    }
+                }
+                var requestUrl = " https://pp-apig.jiomoney.com/payments/jfs/cl/check_collect_link_status ";
+                _httpClient.DefaultRequestHeaders.Add("x-trace-id", "01c570cf-2bdf-49d0-a126-baec7038bbd1");
+                var linkstst = new
+                {
+                    merchantId = merchant_Id,
+                    merchantLinkId = merchantLink_Id,
+                    jioLinkId = jioLink_Id
+                };
+                var jsonlinkstst = JsonConvert.SerializeObject(linkstst);
+                var reques = new StringContent(jsonlinkstst, Encoding.UTF8, "application/json");
+                var respo = await _httpClient.PostAsync(requestUrl, reques);
+                var respon = await respo.Content.ReadAsStringAsync();
+                return respon;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
---
+++ Models/Jiopaymentstatus.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplicationApi.Models
+{
+    public class Jiopaymentstatus
+    {
+        public string merchantId { get; set; }
+        public string merchantLinkId { get; set; }
+        public string jioLinkId { get; set; }
+    }
+}
